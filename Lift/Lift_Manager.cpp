635939516eee0ef18c4fc714ed2339a1a698cd8b
Austin Kugler <aus343@gmail.com>
2018-01-29 17:33:21 -0600

    Fixed LiftManager.cpp to fit coding standards

--- Lift/Lift_Manager.cpp
+++ Lift/Lift_Manager.cpp
@@ -8,51 +8,51 @@ FRC::Lift_Manager::Lift_Manager() :
 
 }
 
-void FRC::Lift_Manager::moveLiftTo(double joyPos)
+void FRC::Lift_Manager::moveLiftTo(double stickY)
 {
 	// Calculate Variables
 	double height = Lift_Motor.GetSelectedSensorPosition(0);
-	Position_Ref = ((joyPos + 1.0) / 2) * MAX_LIFT_POS;
-	Position_Error = Position_Ref - height;
+	positionRef = ((stickY + 1.0) / 2) * MAX_LIFT_POS;
+	positionError = positionRef - height;
 
 	// Calculate Speed_Ref
-	if (abs(Position_Error) < WINDOW)
+	if (abs(positionError) < WINDOW)
 	{
-		Speed_Ref = 0;
+		speedRef = 0;
 	}
-	else if (Position_Error > 0)
+	else if (positionError > 0)
 	{
-		Speed_Ref = Position_Error * UP_GAIN;
-		if (Speed_Ref < UPPER_SPEED_MIN)
+		speedRef = positionError * UP_GAIN;
+		if (speedRef < UPPER_SPEED_MIN)
 		{
-			Speed_Ref = UPPER_SPEED_MIN;
+			speedRef = UPPER_SPEED_MIN;
 		}
 	}
 	else
 	{
-		Speed_Ref = Position_Error * DOWN_GAIN;
-		if (Speed_Ref > LOWER_SPEED_MIN)
+		speedRef = positionError * DOWN_GAIN;
+		if (speedRef > LOWER_SPEED_MIN)
 		{
-			Speed_Ref = LOWER_SPEED_MIN;
+			speedRef = LOWER_SPEED_MIN;
 		}
 	}
 
 	// Limit Speed_Ref
 	// (I understand Talons automatically do this but it is good practice)
-	if (Speed_Ref > 1)
+	if (speedRef > 1)
 	{
-		Speed_Ref = 1;
+		speedRef = 1;
 	}
-	else if (Speed_Ref < -1)
+	else if (speedRef < -1)
 	{
-		Speed_Ref = -1;
+		speedRef = -1;
 	}
 	else
 	{
 		// Ignore
 	}
 
-	Lift_Motor.Set(Speed_Ref);
+	Lift_Motor.Set(speedRef);
 
 }
 
@@ -75,7 +75,7 @@ void FRC::Lift_Manager::resetEnc()
 void FRC::Lift_Manager::updateSD()
 {
 	SmartDashboard::PutNumber("Encoder Height", Lift_Motor.GetSelectedSensorPosition(0));
-	SmartDashboard::PutNumber("Speed Ref", Speed_Ref);
-	SmartDashboard::PutNumber("Position Error", Position_Error);
-	SmartDashboard::PutNumber("Position Ref", Position_Ref);
+	SmartDashboard::PutNumber("Speed Ref", speedRef);
+	SmartDashboard::PutNumber("Position Error", positionError);
+	SmartDashboard::PutNumber("Position Ref", positionRef);
 }
